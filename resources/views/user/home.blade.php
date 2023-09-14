2bb97d6a214ca91ed7dfbf71cf83ab81f7ad28ca
Riwokib <riwellkib@gmail.com>
2023-09-15 00:22:40 +0300

    show enrolled programs and vice versa

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -1,13 +1,16 @@
 <x-app-layout>
-    <h1>Welcome Home User</h1>
+    <div class="flex p-10">
+        <h1 class="text-xl text-sky-500 font-bold">Ongoing Progams</h1>
+    </div>
 
     <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
+        
         <!-- Item 1 -->
         
         @foreach ($programs as $program) 
 
             <div class="bg-gray-50 border border-gray-200 rounded p-6">
-                <div class="flex">
+                <div class="flex justify-between">
                     <img
                         class="hidden w-48 mr-6 md:block"
                         src="images/acme.png"
@@ -15,14 +18,14 @@
                     />
                     <div>
                         <h3 class="text-2xl">
-                            <a href="show.html">{{$program->title}}</a>
+                            <a href="/programs/{{$program->id}}">{{$program->title}}</a>
                         </h3>
                         <div class="text-xl font-bold mb-4">{{$program->physician}}</div>
                         <ul class="flex">
                             @php
                                 $tags = explode(',',$program->tags);
                             @endphp
-                            
+
                             @foreach ($tags as $tag)
                                 <li class="flex items-center justify-center bg-blue-500 text-white rounded-xl py-1 px-3 mr-2 text-xs">
                                     <a href="#">{{$tag}}</a>
@@ -32,6 +35,33 @@
                         <div class="text-lg mt-4">
                             <i class="fa-solid fa-location-dot"></i> {{$program->location}}
                         </div>
+                    </div>
+
+                    <div>
+                        @php
+                            $enrollStatus = '';
+                            $programCounter = 0
+                        @endphp
+
+                        @unless (count($status) == 0)
+                            @foreach ($status as $title)
+                                @if ($title->title == $program->title)
+                                    @php 
+                                        $enrollStatus = $title->title;
+                                        $programCounter += 1
+                                    @endphp
+                                @endif
+                            @endforeach
+                        @else
+                            @php $enrollStatus = 'Not Enrolled' @endphp
+                        @endunless
+
+                        @if($programCounter > 0) <span class="px-2 bg-green-400 rounded-[50%]"></span>
+                        @else @php echo 'Not Enrolled'; @endphp 
+                        @endif
+                        
+                        
+                        {{-- <span class="px-2 bg-red-400 rounded-[50%]"></span> --}}
                     </div>
                 </div>
             </div>
