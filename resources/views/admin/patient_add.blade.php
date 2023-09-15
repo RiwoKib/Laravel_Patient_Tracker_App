2a0ecc77b03912e28f22e87367c3a739571d9ce5
Riwokib <riwellkib@gmail.com>
2023-09-16 01:59:52 +0300

    add patient form view

---
+++ resources/views/admin/patient_add.blade.php
@@ -0,0 +1,204 @@
+<x-app-layout>
+   
+    <section class="flex">
+        {{-- SIDEBAR --}}
+
+        <aside class="z-20 flex-shrink-0 h-screen hidden w-64 overflow-y-auto bg-slate-800 md:block">
+            <div class="bg-slate-900 h-20 w-64 py-2 px-5">
+                <div class="flex space-x-16 bg-green-400 p-1 rounded-md h-12 w-12">
+                    <img src="" alt="">
+
+                    {{-- <div class="flex flex-col">
+                        <h3 class="text-white font-bold uppercase text-base">Manager</h3>
+                        <h3 class="text-white text-light">John Doe</h3>
+                    </div> --}}
+                </div>
+            </div>
+
+            <div class="flex flex-col px-4 py-4 text-gray-400 text-md space-y-3 font-medium">
+                <a href="" class="flex">
+                    <a href="/home" class="uppercase text-gray-600">System Dashboard</a>
+                    <a href="/patient_add" class="flex hover-text-gray-200 space-x-2">Add Patient</a>
+                    <a href="/patients" class="flex hover-text-gray-200 space-x-2">All Patients</a>
+                    <a href="/dose1" class="flex hover-text-gray-200 space-x-2">
+                        <span>Dose Batch 1</span>
+                    </a>
+                    <a href="/dose2" class="flex hover-text-gray-200 space-x-2">
+                        <span>Dose Batch 2</span>
+                    </a>
+                    <a href="/dose3" class="flex hover-text-gray-200 space-x-2">
+                        <span>Dose Batch 3</span>
+                    </a>
+                    <a href="/distributors" class="flex hover-text-gray-200 space-x-2">
+                        <span>Distributors</span>
+                    </a>
+                </a>
+            </div>
+        </aside>
+
+        
+        {{-- CONTENT --}}
+        <main class="bg-slate-200 h-screen w-full overflow-y-auto">
+            <div class="p-8">
+
+                <div class="py-5">
+                    <h1 class="text-xl font-bold">Add Patient</h1>
+                    <p class="text-sm">Your new patient</p>
+                </div>
+                
+                {{-- ADD PATIENT FORM --}}
+                <section class=" py-1 bg-blueGray-50">
+                    <div class="w-full lg:w-8/12 px-4 mx-auto mt-6">
+                        <div class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-100 border-0">
+                        <div class="rounded-t bg-white mb-0 px-6 py-6">
+                            <div class="text-center flex justify-between">
+                            <h6 class="text-blueGray-700 text-xl font-bold">
+                                Details
+                            </h6>
+                            <button class="bg-slate-700 text-white active:bg-slate-400 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150" type="submit">
+                                Submit
+                            </button>
+                            </div>
+                        </div>
+                        <div class="flex-auto px-4 lg:px-10 py-10 pt-0">
+                            <form>
+                            <h6 class="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
+                                Patient Information
+                            </h6>
+                            <div class="flex flex-wrap">
+                                <div class="w-full lg:w-6/12 px-4">
+                                <div class="relative w-full mb-3">
+                                    <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
+                                    First Name
+                                    </label>
+                                    <input type="text" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" placeholder="Riwell">
+                                </div>
+                                </div>
+                                <div class="w-full lg:w-6/12 px-4">
+                                <div class="relative w-full mb-3">
+                                    <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
+                                    Last Name
+                                    </label>
+                                    <input type="text" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" placeholder="Kib">
+                                </div>
+                                </div>
+                                <div class="w-full lg:w-4/12 px-4">
+                                    <div class="relative w-full mb-3">
+                                        <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
+                                        Age
+                                        </label>
+                                        <input type="text" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" placeholder="18+">
+                                    </div>
+                                </div>
+                                <div class="w-full lg:w-4/12 px-4">
+                                    <div class="relative w-full mb-3">
+                                        <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
+                                        ID No.
+                                        </label>
+                                        <input type="text" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" placeholder="36864595">
+                                    </div>
+                                </div>
+                                <div class="w-full lg:w-4/12 px-4">
+                                    <div class="relative w-full mb-3">
+                                        <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
+                                        Dose Type
+                                        </label>
+                                        <select type="text" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150">
+                                            <option value="1">DOSE 1</option>
+                                            <option value="2">DOSE 2</option>
+                                            <option value="3">DOSE 3</option>
+                                            <option value="4">FOC</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                
+                            </div>
+                    
+                            <hr class="mt-6 border-b-1 border-gray-300">
+                    
+                            <h6 class="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
+                                Contact Information
+                            </h6>
+                            <div class="flex flex-wrap">
+                                <div class="w-full lg:w-4/12 px-4">
+                                    <div class="relative w-full mb-3">
+                                        <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
+                                        Phone Number
+                                        </label>
+                                        <input type="text" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" placeholder="0701-234-567">
+                                    </div>
+                                </div> 
+                                <div class="w-full lg:w-12/12 px-4">
+                                <div class="relative w-full mb-3">
+                                    <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
+                                    Address
+                                    </label>
+                                    <input type="text" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" placeholder="Langata, Park 2 Estate">
+                                </div>
+                                </div>
+                                <div class="w-full lg:w-4/12 px-4">
+                                <div class="relative w-full mb-3">
+                                    <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
+                                    City
+                                    </label>
+                                    <input type="email" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" placeholder="Nairobi">
+                                </div>
+                                </div>
+                                <div class="w-full lg:w-4/12 px-4">
+                                <div class="relative w-full mb-3">
+                                    <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
+                                    Country
+                                    </label>
+                                    <input type="text" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" placeholder="Kenya">
+                                </div>
+                                </div>
+                                <div class="w-full lg:w-4/12 px-4">
+                                <div class="relative w-full mb-3">
+                                    <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
+                                    Postal Code
+                                    </label>
+                                    <input type="text" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" placeholder="00510">
+                                </div>
+                                </div>
+                            </div>
+                    
+                            <hr class="mt-6 border-b-1 border-gray-300">
+                    
+                            <h6 class="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
+                                Next of Kin
+                            </h6>
+                            <div class="flex flex-wrap">
+                                <div class="w-full lg:w-6/12 px-4">
+                                    <div class="relative w-full mb-3">
+                                        <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
+                                        First Name
+                                        </label>
+                                        <input type="text" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" placeholder="Firstname">
+                                    </div>
+                                </div>
+                                <div class="w-full lg:w-6/12 px-4">
+                                    <div class="relative w-full mb-3">
+                                        <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
+                                        Last Name
+                                        </label>
+                                        <input type="text" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" placeholder=" Lastname">
+                                    </div>
+                                </div>
+                                <div class="w-full lg:w-4/12 px-4">
+                                    <div class="relative w-full mb-3">
+                                        <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
+                                        Next of Kin Number
+                                        </label>
+                                        <input type="text" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" placeholder="0701-234-567">
+                                    </div>
+                                </div>
+                            </div>
+                            </form>
+                        </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </main>
+    </section>
+</x-app-layout>
